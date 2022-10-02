e9e2a53f3e8150046f3b6e875cea106a0f300695
Gowri Harish <gharish@Purdue.edu>
2022-10-02 00:25:48 -0400

    Reset Password

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import Signup from "./components/SignUp";
 import Login from "./components/Login";
 import Main from "./components/Main";
 import EmailVerify from "./components/EmailVerify";
+import ForgotPassword from "./components/ForgotPassword";
+import PasswordReset from "./components/PasswordReset";
  
 const App = () => {
   const user = localStorage.getItem("token");
@@ -29,6 +31,8 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/" exact element={<Navigate replace to="/login" />} />
       <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
      </Routes>
    </div>
  );
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(require("./routes/record"));
 app.use(require("./routes/users"));
 app.use(require("./routes/auth"));
+app.use(require("./routes/passwordReset"));
 // get driver connection
 const dbo = require("./db/conn");
 
