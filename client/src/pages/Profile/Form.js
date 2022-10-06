01fe671ce0f9a6b623cc54da5994b1cb583b0ffc
Aayushya Nair <nair61@purdue.edu>
2022-10-06 14:59:23 -0400

    mutliple experinces in form ui

---
+++ client/src/pages/Profile/Form.js
@@ -0,0 +1,375 @@
+
+import Grid from '@mui/material/Grid';
+import {IconButton, FormGroup, Box, MenuItem, InputLabel, Select, FormControl, Card, TextField, Typography, CardContent, Button, FormControlLabel, Checkbox } from '@mui/material';
+import RemoveCircleOutlineRoundedIcon from '@mui/icons-material/RemoveCircleOutlineRounded';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+import MaxWidthDialog from '../../components/DialogueBox/Confirmation';
+//import { useNavigate } from "react-router";
+
+export  default function Form() {
+ const [form, setForm] = useState({
+   first_name: "",
+   last_name: "",
+   email: "",
+   phone: "",
+   address: "",
+ });
+
+ const [experiences, setExperiences] = useState([
+  { id: uuidv4(), employerName: "", position: "", 
+  location: "", startDate: "", endDate: "", description: "" },
+]);
+
+
+const handleChangeExperience = (id, event) => {
+  const newExperiences = experiences.map(i => {
+    if(id === i.id) {
+      i[event.target.name] = event.target.value;
+    }
+    return i;
+  })
+  setExperiences(newExperiences);
+}
+
+const handleAddExperiences = () => {
+  setExperiences([...experiences, { id: uuidv4(),  employerName: '', position: '', 
+  location: '', startDate: '', endDate: '', description: '' }])
+}
+
+const handleRemoveExperiences = (id) => {
+  const values  = [...experiences];
+  values.splice(values.findIndex(value => value.id === id), 1);
+  setExperiences(values);
+}
+ //const navigate = useNavigate();
+ 
+ // These methods will update the state properties.
+ function updateForm(value) {
+  console.log('I was triggered during updateForm'); 
+  return setForm((prev) => {
+     return { ...prev, ...value };
+   });
+ }
+ 
+ // This function will handle the submission.
+ async function onSubmit(e) {
+   e.preventDefault();
+   console.log('I was triggered during onSubmit');
+ 
+   // When a post request is sent to the create url, we'll add a new record to the database.
+   const newPerson = { ...form };
+  
+   await fetch("http://localhost:5000/dbprofile/add", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(newPerson),
+   })
+   .catch(error => {
+     window.alert(error);
+     return;
+   });
+
+   
+ 
+   setForm({ first_name: "",
+   last_name: "",
+   email: "",
+   phone: "",
+   address: ""});
+   
+   //navigate("/");
+ }
+ 
+  return (
+    
+    <Grid mx ={35}>
+      
+      <Box my={10}>
+      </Box>
+      
+      <form onSubmit={onSubmit}>
+      <Typography gutterBottom variant="h5">
+        Profile
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in your personal information.
+      </Typography>
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item className="form-group">
+            <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required  value={form.first_name}
+           onChange={(e) => setForm({...form, first_name: e.target.value})}/>
+          </Grid>
+          <Grid xs={12} sm={6} item className="form-group">
+            <TextField id ="last_name" className="form-control" placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required value={form.last_name}
+           onChange={(e) => setForm({...form, last_name: e.target.value})}/>
+          </Grid>
+          <Grid item xs={12} className="form-group">
+            <TextField id ="email" className="form-control" type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required value={form.email}
+           onChange={(e) => setForm({...form, email: e.target.value})}/>
+          </Grid>
+          <Grid item xs={12} className="form-group">
+            <TextField id ="phone" className="form-control" type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required value={form.phone}
+           onChange={(e) => setForm({...form, phone: e.target.value})}/>
+          </Grid>
+          <Grid item xs={12} className="form-group">
+            <TextField id ="address" className="form-control" placeholder="Enter address" label="Address" variant="outlined" fullWidth required value={form.address}
+           onChange={(e) => setForm({...form, address: e.target.value})}/>
+          </Grid>
+        </Grid>
+        <Box my={10}>
+      </Box>
+
+      <Typography gutterBottom variant="h5">
+        Education
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in your education information.
+      </Typography>
+
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item>
+            <TextField placeholder="Purdue University" label="Insitute Name" variant="outlined" fullWidth  />
+          </Grid>
+          <Grid xs={12} sm={6} item>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Degree</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Degree"
+              >
+                <MenuItem value={10}>Bachelors</MenuItem>
+                <MenuItem value={20}>Masters</MenuItem>
+                <MenuItem value={30}>PHD</MenuItem>
+              </Select>
+            </FormControl>                
+            </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField placeholder="Software Engineering" label="Major" variant="outlined" fullWidth  />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField placeholder="Art and Design Studio" label="Minor" variant="outlined" fullWidth  />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <TextField type="number" placeholder="4.0" label="GPA" variant="outlined" fullWidth  />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="Start date"
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="End date"
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+        </Grid>
+
+
+      <Box my={10}>
+      </Box>
+
+      <Typography gutterBottom variant="h5">
+        Experiences
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in your education information.
+      </Typography>
+
+      {experiences.map(experience => (
+          <div key={experience.id}>
+            <Grid container spacing={1}>
+            <Grid item xs={12} sm={6}>
+            <TextField placeholder="Google" 
+              label="Employer Name" 
+              variant="outlined" fullWidth
+              name="firstName"
+              //value = {experience.employerName}
+              //onChange= {(e) => handleChangeExperience(experience.id, e)}
+             // onChange={event => handleChangeInput(inputField.id, event)}
+              //onChange={(e) => handleChangeInput({ firstName: e.target.value })}
+            />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <TextField placeholder="Software Engineer" 
+              label="Position" 
+              variant="outlined" fullWidth
+              name="lastName"
+              //value = {experience.position}
+              //onChange= {(e) => handleChangeExperience(experience.id, e)}
+             //onChange={(e) => handleChangeInput({ lastName: e.target.value })}
+            />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <TextField placeholder="San Diego" 
+              label="Location" 
+              variant="outlined" fullWidth
+              name="lastName"
+              //value={experience.location}
+              //onChange={event => handleChangeInput(inputField.id, event)}
+             //onChange={(e) => handleChangeInput({ lastName: e.target.value })}
+            />
+            </Grid>
+            <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="I currently work here" />
+            </FormGroup>
+            <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="Start date"
+              //value={experience.startDate}
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="End date"
+              //value={experience.endDate}
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+          <Grid item xs={12}>
+            <TextField multiline rows={4} placeholder="" label="Description" 
+            variant="outlined" fullWidth
+            //value={experience.description} 
+            />
+          </Grid>
+            </Grid>
+            <IconButton disabled={experience.length === 1} 
+            onClick={() => handleRemoveExperiences(experience.id)}
+            >
+              <RemoveCircleOutlineRoundedIcon />
+            </IconButton>
+            <IconButton
+              onClick={handleAddExperiences}
+            >
+              <AddCircleOutlineRoundedIcon />
+            </IconButton>
+          </div>
+        )) }
+
+
+
+      <Box my={10}>
+      </Box>
+
+      <Typography gutterBottom variant="h5">
+        Projects
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in your projects information.
+      </Typography>
+
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <TextField placeholder="Chefly" label="Project Name" variant="outlined" fullWidth  />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="Start date"
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="date"
+              label="End date"
+              type="date"
+              defaultValue="2017-05-24"
+              sx={{ width: 220 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />       
+          </Grid>
+          <Grid item xs={12}>
+            <TextField multiline rows={4} placeholder="" label="Description" variant="outlined" fullWidth  />
+          </Grid>
+        </Grid>
+
+
+
+      <Box my={10}>
+      </Box>
+
+      <Typography gutterBottom variant="h5">
+        Skills
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in your skills.
+      </Typography>
+
+        <Grid item xs={12} sm={6}>
+          <TextField placeholder="Java" label="Skills" variant="outlined" fullWidth />
+        </Grid>
+
+
+      <Box my={10}>
+      </Box>
+
+      <Typography gutterBottom variant="h5">
+        Links
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+        Please fill in any links you would like to share.
+      </Typography>
+
+        <Grid item xs={12} sm={6}>
+          <TextField placeholder="Java" label="Links" variant="outlined" fullWidth />
+        </Grid>
+        
+        <Box my={10}>
+      </Box>
+        <Grid item xs={12} sm={6}>
+          <Button type="submit" variant="contained" color="primary" >Submit </Button>
+          
+        </Grid>
+
+      </form>
+
+      
+
+      <Box my={10}>
+      </Box>
+
+    </Grid>
+  )
+}
+
+
