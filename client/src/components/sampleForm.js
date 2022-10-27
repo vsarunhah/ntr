4201fbda09dca176f11afef40073cc27c7d8914c
Pranjal Punekar <ppunekar@purdue.edu>
2022-10-27 00:08:53 -0400

    trying to pull

--- client/src/components/sampleForm.js
+++ client/src/components/sampleForm.js
@@ -14,7 +14,26 @@ function SampleForm () {
       end_date: "", 
       current_job: "",
       description: "",
-      location: ""
+      location: "",
+    },
+  ])
+  const [skills, setSkills] = useState([
+    {
+      skill: "",
+    }
+  ])
+  const [links, setLinks] = useState([
+    {
+      link: "",
+    }
+  ])
+  const [projects, setProjects] = useState([
+    {
+      id: uuidv4(), 
+      name: "", 
+      start_date: "", 
+      end_date: "",
+      description: ""
     },
   ])
 
@@ -43,16 +62,35 @@ function SampleForm () {
     data[index][event.target.name] = event.target.value;
     setEducations(data);
   }
+  
+  const handleProjectChange = (event, index) => {
+    let data = [...projects];
+    data[index][event.target.name] = event.target.value;
+    setProjects(data);
+  }
 
+  const handleSkillChange = (event, index) => {
+    let data = [...skills];
+    data[index] = event.target.value;
+    setSkills(data);
+  }
+  const handleLinkChange = (event, index) => {
+    let data = [...links];
+    data[index] = event.target.value;
+    setLinks(data);
+  }
   const submit = async (e) => {
     e.preventDefault();
     console.log(experiences);
     console.log(educations);
+    console.log(projects);
     // SEND EXPERIENCES AND EDUCATIONS TO SERVER
     const data = {
       user_id: localStorage.getItem('user_id'),
       experiences: experiences,
       educations: educations,
+      projects: projects,
+      skills: skills
     }
     try {
       await axios
@@ -66,11 +104,27 @@ function SampleForm () {
     // RETRIEVE EXPERIENCES
     try {
       const { data: res } = await axios.post("http://localhost:5000/profile/get_experiences", data);
-      console.log(data);
-      console.log(res);
-      console.log(res.data);
-      console.log(res.data);
+      console.log("experiences retreived:");
+      console.log(data.experiences);
       // data.map((i)=> {console.log("i : ", i)});
+    } catch (error) {
+      console.log(error);
+      console.log("oops");
+    }
+    //getting educations
+    try {
+      const { data: res } = await axios.post("http://localhost:5000/profile/get_educations", data);
+      console.log("educations retreived : ");
+      console.log(data.educations);
+    } catch (error) {
+      console.log(error);
+      console.log("oops");
+    }
+    //getting educations
+    try {
+      const { data: res } = await axios.post("http://localhost:5000/profile/get_projects", data);
+      console.log("projects retreived : ");
+      console.log(data.projects);
     } catch (error) {
       console.log(error);
       console.log("oops");
@@ -90,6 +144,31 @@ function SampleForm () {
     }
 
     setExperiences([...experiences, object])
+  } 
+
+  const addProjects = () => {
+    let object = {
+      id: uuidv4(),
+      name: "", 
+      start_date: "", 
+      end_date: "",
+      description: "",
+    }
+
+    setProjects([...projects, object])
+  }
+
+  const addSkills = () => {
+    let object = {
+      skill : ""
+    }
+    setSkills([...skills, object])
+  }
+  const addLinks = () => {
+    let object = {
+      link : ""
+    }
+    setLinks([...links, object])
   }
 
   const addEducations = () => {
@@ -108,6 +187,7 @@ function SampleForm () {
     setEducations([...educations, object])
   }
 
+
   const removeExperiences = (index) => {
     let data = [...experiences];
     data.splice(index, 1)
@@ -119,7 +199,21 @@ function SampleForm () {
     data.splice(index, 1)
     setEducations(data)
   }
-
+  const removeProjects = (index) => {
+    let data = [...projects];
+    data.splice(index, 1);
+    setProjects(data);
+  }
+  const removeSkills = (index) => {
+    let data = [...skills];
+    data.splice(index, 1);
+    setSkills(data);
+  }
+  const removeLinks = (index) => {
+    let data = [...links];
+    data.splice(index, 1);
+    setLinks(data);
+  }
   return (
     <div className="App">
       <form onSubmit={submit}>
@@ -141,7 +235,7 @@ function SampleForm () {
               <button onClick={() => removeExperiences(index)}>Remove</button>
             </div>
           )
-        })}
+        })} 
       </form>
       <button onClick={addExperiences}>Add Experience..</button>
       <br />
@@ -167,6 +261,29 @@ function SampleForm () {
         })}
       </form>
       <button onClick={addEducations}>Add Education..</button>
+      <br />
+      <form onSubmit={submit}>
+        {projects.map((form, index) => {
+          return (
+            <div key={index}>
+              <input
+                name='name'
+                placeholder='chefly'
+                onChange={event => handleProjectChange(event, index)}
+                value={form.name}
+              />
+              <input
+                name='description'
+                placeholder='Degree'
+                onChange={event => handleProjectChange(event, index)}
+                value={form.age}
+              />
+              <button onClick={() => removeProjects(index)}>Remove</button>
+            </div>
+          )
+        })}
+      </form>
+      <button onClick={addProjects}>Add Projects..</button>
       <br />
       <button onClick={submit}>Submit</button>
     </div>
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -10,7 +10,14 @@ router.route("/profile/add").post(async function (req, res) {
         let update_query = {
             $set: {
                 experiences: req.body.experiences,
-                educations: req.body.educations
+                educations: req.body.educations,
+                projects: req.body.projects,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                profileEmail: req.body.profileEmail,
+                phoneNumber: req.body.phoneNumber,
+                links: req.body.links,
+                skills: req.body.skills,
             },
           };
         user = await User.updateOne({_id: req.body.user_id}, update_query);
@@ -37,5 +44,29 @@ router.route("/profile/get_experiences").post(async function (req, res) {
         res.status(500).send('Server Error');
     }
 });
+router.route("/profile/get_educations").post(async function (req, res) {
+    console.log("profile/get_educations HERE");
+    console.log(req.body);
+    try {
+        let user = await User.findOne({_id: req.body.user_id}).select("educations");
+        console.log("user:", user);
 
+        res.json(user.educations);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+router.route("/profile/get_projects").post(async function (req, res) {
+    console.log("profile/get_projects HERE");
+    console.log(req.body);
+    try {
+        let user = await User.findOne({_id: req.body.user_id}).select("projects");
+        console.log("user:", user);
+        res.json(user.projects);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
