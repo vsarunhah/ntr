fb0ca0d59a04decd4a14a7dff3883b0cbe4e3129
Varun Shah <varunshah660@gmail.com>
2022-11-03 16:58:34 -0400

    Filtering all applications

--- server/routes/applications.js
+++ server/routes/applications.js
@@ -14,78 +14,8 @@ const statuses = {
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const applicationRoutes = express.Router();
 
-// This section will help you get a list of all the applications.
-// applicationRoutes.route("/applications").post(function (req, res) {
-//   let db_connect = dbo.getDb("employees");
-//   let user_id = ObjectId(req.body.user_id);
-//   let status_body = req.body["status"];
-//   let find_query = { user_id: user_id };
-//   //  console.log(status_body);
-//   if (Object.keys(statuses).includes(status_body)) {
-//     find_query["status"] = statuses[status_body];
-//     console.log("find query: ", find_query);
-//   }
-//   db_connect
-//     .collection("applications")
-//     .find(find_query)
-//     .toArray(function (err, result) {
-//       if (err) throw err;
-//       for (let i = 0; i < result.length; i++) {
-//         result[i].application_id = i;
-//       }
-//       res.json(result);
-//       //  console.log(result);
-//     });
-// });
-
-// This section will help you update the status of an application.
-// applicationRoutes.route("/application/update").post(function (req, res) {
-//   let db_connect = dbo.getDb("employees");
-//   let user_id = ObjectId(req.body.user_id);
-//   let application_id = ObjectId(req.body.application_id);
-//   let update_query = {
-//     $set: {
-//       status: req.body.status,
-//     },
-//   };
-//   db_connect
-//     .collection("applications")
-//     .updateOne(
-//       { _id: application_id, user_id: user_id },
-//       update_query,
-//       function (err, result) {
-//         if (err) throw err;
-//         res.json(result);
-//         res.status(200).send();
-//       }
-//     );
-// });
-
-// This section will help you delete an application.
-// applicationRoutes.route("/application/delete").post(function (req, res) {
-//   let db_connect = dbo.getDb("employees");
-//   let user_id = ObjectId(req.body.user_id);
-//   let application_id = ObjectId(req.body.application_id);
-//   db_connect
-//     .collection("applications")
-//     .deleteOne(
-//       { _id: application_id, user_id: user_id },
-//       function (err, result) {
-//         if (err) throw err;
-//         res.json(result);
-//         if (result.deletedCount == 1) {
-//           res.status(200).send();
-//         } else {
-//           res.status(500).send();
-//         }
-//       }
-//     );
-// });
-
 // This section will help you create a new application.
 applicationRoutes.route("/application/create").post(async function (req, res) {
-  console.log("application create HERE");
-  console.log(req.body);
   let myobj = {
     id: crypto.randomUUID(),
     companyName: req.body.companyName,
@@ -103,40 +33,42 @@ applicationRoutes.route("/application/create").post(async function (req, res) {
 });
 
 applicationRoutes.route("/applications").post(async function (req, res) {
-  console.log("application get HERE");
-  console.log(req.body);
     try {
-      if (req.body.status == 'All') {
+      if (req.body["status"] === 'All' || req.body["company_name"] === 'All' || req.body["title"] === 'All' || req.body["date_applied"] === 'All') {
         let user = await User.findOne({_id: req.body.user_id}).select('applications');
         for (let i = 0; i < user.applications.length; i++) {
           user.applications[i].application_id = i;
         }
-        console.log(user.applications);
         res.status(200).send({data: user.applications, message: "Applications retrieved"});
       } else {
-        let status = statuses[req.body.status];
-        // let user = await User.findOne({ _id: req.body.user_id, "applications.applicationStatus": status}).select('applications');
-        let user = await User.findOne({ _id: req.body.user_id}).select('applications');
+        let filt = { _id: req.body.user_id};
+        let user = await User.findOne(filt).select('applications');
         for (let i = 0; i < user.applications.length; i++) {
           user.applications[i].application_id = i;
         }
-        let applications = user.applications.filter(application => application.applicationStatus == status);
-        console.log("filtered applications: ", applications);
+        let applications = user.applications;
+        if (req.body["status"] !== undefined) {
+          applications = user.applications.filter(application => application.applicationStatus == statuses[req.body["status"]]);
+        } else if (req.body["company_name"] !== undefined) {
+          applications = user.applications.filter(application => application.companyName == req.body["company_name"]);
+        } else if (req.body["title"] !== undefined) {
+          applications = user.applications.filter(application => application.roleName == req.body["title"]);
+        } else if (req.body["date_applied"] !== undefined) {
+          applications = user.applications.filter(application => application.applicationDate == req.body["date_applied"]);
+        }
         if (applications.length > 0) {
-          console.log("applications: ", applications);
           res.status(200).send({data: applications, message: "Applications found"});
         } else {
           res.status(200).send({data: [], message: "No applications found"});
         }
       }
     } catch (err) {
+      console.log(err);
       res.status(500).send({ message: err.message });
     }
 });
 
 applicationRoutes.route("/application/delete").post(async function (req, res) {
-  console.log("application delete HERE");
-  console.log(req.body);
   try {
     let arr = await User.findOne({ _id: req.body.user_id }).select("applications");
     
@@ -158,8 +90,6 @@ applicationRoutes.route("/application/delete").post(async function (req, res) {
 });
 
 applicationRoutes.route("/application/update").post(async function (req, res) {
-  console.log("application update HERE");
-  console.log(req.body);
   let update_query = {
     $set: {
       "applications.$.applicationStatus": req.body.status,
