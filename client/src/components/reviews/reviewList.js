fda07d582961ca8eee640eb092f26ffe49a82833
Varun Shah <varunshah660@gmail.com>
2022-11-08 16:39:17 -0500

    Filter by tag

--- client/src/components/reviews/reviewList.js
+++ client/src/components/reviews/reviewList.js
@@ -7,6 +7,22 @@ import Navbar from "../../components/Navbar/Navbar";
 import { Review } from "./reviewCard";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 export default function ReviewList() {
   const [reviews, setReviews] = useState([]);
@@ -42,18 +58,44 @@ export default function ReviewList() {
     setReviews(newReviews);
   }
 
-  const filterReviews = (query, reviews) => {
-    if (!query) {
+  const tagList = [
+    "Compensation",
+    "Culture",
+    "Work/Life Balance",
+    "Benefits",
+    "Management",
+    "Career Growth",
+    "Diversity",
+  ];
+
+  const filterReviews = (query, tags, reviews) => {
+    if (!query && (!tags || tags.length == 0)) {
       return reviews;
     } else {
-      return reviews.filter((review) =>
-        review.companyName.toLowerCase().includes(query)
-      );
+      let search = reviews;
+      if (query) {
+        search = search.filter((review) =>
+          review.companyName.toLowerCase().includes(query)
+        );
+      }
+      if (tags && tags.length != 0) {
+        search = search.filter((review) => {
+          let found = false;
+          Array.from(tags).forEach((tag) => {
+            if (review.tags.includes(tag)) {
+              found = true;
+            }
+          });
+          return found;
+        });
+      }
+      return search;
     }
   };
 
   const [searchQuery, setSearchQuery] = useState("");
-  const filteredReviews = filterReviews(searchQuery, reviews);
+  const [tagQuery, updateTagQuery] = useState([]);
+  const filteredReviews = filterReviews(searchQuery, tagQuery, reviews);
 
   return (
     <Grid mx={25}>
@@ -74,7 +116,8 @@ export default function ReviewList() {
             label="Search by company name"
           />
         </Grid>
-        <Grid item sx={{ position: "absolute", top: 157, right: 800 }}>
+
+        <Grid item sx={{ }}>
           <Box
             sx={{
               display: "flex",
@@ -88,6 +131,24 @@ export default function ReviewList() {
           >
             <SearchIcon fontSize="medium" />
           </Box>
+        </Grid>
+        <Grid>
+        <FormControl sx={{ width: 300, marginLeft: '5vw', marginTop: '1.75vh' }}>
+            <InputLabel>Tags</InputLabel>
+            <Select
+              multiple
+              value={Array.from(tagQuery)}
+              onChange={(e) => updateTagQuery(Array.from(e.target.value))}
+              input={<OutlinedInput label="Tags" />}
+              MenuProps={MenuProps}
+            >
+              {tagList.map((tag) => (
+                <MenuItem key={tag} value={tag}>
+                  {tag}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Grid>
       </Grid>
       <ul>
